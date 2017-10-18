f3adc24ee90ea576b510c69fe6f9b601591b541d
Mark Delos Reyes <makizworks@gmail.com>
2017-10-17 23:31:40 -0400

    add listener on square

--- script.js
+++ script.js
@@ -14,5 +14,13 @@ var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < square.length; i++) {
+    //add initial colors to squares
     square[i].style.backgroundColor = colors[i];
+
+    //add click listeners to square
+    square[i].addEventListener("click", function() {
+        //grab color of click square
+        console.log(this.style.backgroundColor)
+        //compare color to pickedColor
+    });
 }
