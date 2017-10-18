ca27f4b3871b3376f2a4685bd87e57cb3cb04302
Mark Delos Reyes <makizworks@gmail.com>
2017-10-17 23:18:33 -0400

    add square color using script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("color game js in")
+var colors = [
+    "rgb(255, 0, 0)", //red
+    "rgb(0, 255, 0)", //green
+    "rgb(255, 255, 0)", //yellow
+    "rgb(0, 0, 255)", //blue
+    "rgb(0, 255, 255)", //cyan
+    "rgb(255, 0, 255)", //magenta
+]
+
+var square = document.querySelectorAll(".square")
+
+for (var i = 0; i < square.length; i++) {
+    square[i].style.backgroundColor = colors[i];
+}
