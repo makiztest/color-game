db42b1db6cd35fc5518f5b57e382a682a8033f47
Mark Delos Reyes <makizworks@gmail.com>
2017-10-18 00:09:18 -0400

    change all square color when picked the correct color

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var colors = [
-    "rgb(255, 0, 0)", //red
-    "rgb(0, 255, 0)", //green
-    "rgb(255, 255, 0)", //yellow
-    "rgb(0, 0, 255)", //blue
-    "rgb(0, 255, 255)", //cyan
-    "rgb(255, 0, 255)", //magenta
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 0, 255)",
 ]
 
 var square = document.querySelectorAll(".square")
@@ -20,12 +20,18 @@ for (var i = 0; i < square.length; i++) {
     square[i].addEventListener("click", function() {
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
-            //when picked correct color show message correct
-            messageDisplay.textContent = "correct"
+            messageDisplay.textContent = "correct";
+            changeAllColor(clickedColor);
         } else {
-            //when picked wrong color show message try again
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "try again";
         }
     });
+}
+
+function changeAllColor(color) {
+    //change all square color picked the correct color
+    for (var i = 0; i < square.length; i++) {
+        square[i].style.backgroundColor = color;
+    }
 }
